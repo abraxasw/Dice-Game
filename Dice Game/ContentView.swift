505aa4496182f144c955249ae10e852907e3d7ea
Andrew Tam <me@andrewhtam.com>
2024-11-13 09:26:45 +1100

    Initial Commit

---
+++ Dice Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dice Game
+//
+//  Created by Andrew Tam on 13/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Dice!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
